86e4d61e11604f38169e1a3ed368093333e51860
GubarevV <GubarevV@DESKTOP-VJV0OJ6>
2024-01-15 12:47:56 +0500

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using Microsoft.Win32;
+using FellowOakDicom;
+using FellowOakDicom.Imaging;
+using System.Windows.Media.Imaging;
+using EvilDICOM.Core.IO.Reading;
+using FellowOakDicom.Imaging.Render;
+using EvilDICOM.Core;
+using EvilDICOM.Core.Element;
+using FellowOakDicom.IO.Buffer;
+using EvilDICOM.Core.IO.Data;
+using EvilDICOM.Core.Enums;
+using EvilDICOM.Core.Interfaces;
+
+
+
+namespace DicomWpf
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadDICOMButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "DICOM Files (*.dcm)|*.dcm|All Files (*.*)|*.*";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string filePath = openFileDialog.FileName;
+
+                var dicomFile = DicomFile.Open(filePath);
+                var dicomImage = new DicomImage(dicomFile.Dataset);
+
+                // Пример конвертации изображения в массив байтов
+                byte[] imageBytes = ConvertDicomImageToBytes(dicomImage);
+
+                // Пример обработки изображения с использованием вашей нейросети
+                var processedResult = NeuralNetwork.ProcessImage(imageBytes);
+
+                // Отображение обработанного изображения
+                DisplayImage(imageBytes);
+
+                // Вывод результата
+                resultText.Text = "Result: " + processedResult.ToString();
+            }
+        }
+
+
+
+
+        private void DisplayImage(byte[] imageBytes)
+        {
+            BitmapImage bitmap = new BitmapImage();
+            bitmap.BeginInit();
+            bitmap.CacheOption = BitmapCacheOption.OnLoad;
+            bitmap.StreamSource = new MemoryStream(imageBytes);
+            bitmap.EndInit();
+
+            dicomImage.Source = bitmap;
+        }
+        private byte[] ConvertDicomImageToBytes(DicomImage dicomImage)
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                // Получаем объект DicomPixelData изображения
+                var dicomPixelData = dicomImage.RenderImage().As<DicomPixelData>();
+
+                // Получаем значения пикселей в виде массива байтов
+                var byteArray = dicomPixelData.GetFrame(0).Data;
+
+                // Записываем массив байтов в поток
+                memoryStream.Write(byteArray, 0, byteArray.Length);
+
+                return memoryStream.ToArray();
+            }
+        }
+
+      
+    }
+}
---
+++ NeuralNetwork.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.ML.Data;
+using Microsoft.ML;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DicomWpf
+{
+    internal class NeuralNetwork
+    {
+        // Класс для представления входных данных
+        public class ImageData
+        {
+            [LoadColumn(0)]
+            public byte Label;
+
+            [LoadColumn(1, 783)]
+            [VectorType(784)]
+            public float[] PixelValues;
+        }
+
+        // Класс для представления предсказания
+        public class Prediction
+        {
+            [ColumnName("PredictedLabel")]
+            public byte Digit;
+        }
+
+        // Метод для обработки изображения с использованием ML.NET
+        public static string ProcessImage(byte[] imageBytes)
+        {
+            // Создание объекта MLContext для работы с ML.NET
+            var mlContext = new MLContext();
+
+            // Загрузка модели (замените "path/to/your/model.zip" на реальный путь к вашей модели)
+            var model = mlContext.Model.Load("path/to/your/model.zip", out _);
+
+            // Создание представления изображения
+            var imageData = new ImageData
+            {
+                PixelValues = imageBytes.Select(b => (float)b / 255.0f).ToArray()
+            };
+
+            // Прогнозирование
+            var predictionEngine = mlContext.Model.CreatePredictionEngine<ImageData, Prediction>(model);
+            var prediction = predictionEngine.Predict(imageData);
+
+            // Здесь вы можете использовать prediction.Digit в зависимости от вашей задачи
+            // Например, вернем строку "Predicted Digit: {prediction.Digit}"
+            return $"Predicted Digit: {prediction.Digit}";
+        }
+    }
+}
